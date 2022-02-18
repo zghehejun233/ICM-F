f9bc332e8ce9f9a6fdf2e879d8f8d5fdd9502116
guosurui <13012615613@163.com>
2022-02-18 22:02:28 +0800

    fix: 修正数据结构
    修正数据状态

--- src/data_manager.py
+++ src/data_manager.py
@@ -8,14 +8,16 @@
 
 import logging
 import pandas as pd
+import numpy as np
 import config
 
 
 def global_equity_model_init():
-    config.happiness_index
+    # config.happiness_index
 
     human_development_file = pd.read_csv("/Users/guosurui/Documents/git_code/ICM-F/data/global_equity_model/人类发展指数.csv")
-    config.development_index = human_development_file.values
+    temp = human_development_file.values
+    config.development_index = temp[config.china_index]
     logging.debug(config.development_index)
 
     labor_file = pd.read_csv("/Users/guosurui/Documents/git_code/ICM-F/data/global_equity_model/劳动力.csv")
@@ -32,8 +34,8 @@ def global_equity_model_init():
     logging.debug(config.education_index)
 
     non_resident_patent = pd.read_csv("/Users/guosurui/Documents/git_code/ICM-F/data/global_equity_model/非居民专利数.csv")
-    config.technology_index = non_resident_patent
-    logging.debug(config.technology_index)
+    config.patents_per_capitan = non_resident_patent
+    logging.debug(config.patents_per_capitan)
 
     power_used = pd.read_csv("/Users/guosurui/Documents/git_code/ICM-F/data/global_equity_model/耗电量.csv")
     config.electricity_per_capitan = power_used.values
@@ -57,6 +59,47 @@ def global_equity_model_init():
     logging.debug(config.forest_per_capitan)
 
 
+'''
 # 数据清洗
 def global_equity_model_data_clean():
-    pass
+    # 定义反复使用的remove_list
+    remove_list = []
+
+    # 处理发展指数
+    for i in range(3, 26, 2):
+        remove_list.append(i)
+    for j in range(27, 35):
+        remove_list.append(j)
+    config.development_index = np.delete(config.development_index, remove_list, axis=1)
+    remove_list = []
+
+    # 处理劳动力指数
+    for i in range(4, 34):
+        remove_list.append(i)
+    config.labor_index = np.delete(config.labor_index, remove_list, axis=1)
+    remove_list = []
+
+    # 处理GDP指数
+
+    # 处理高等教育水平指数
+    for i in range(4, 14):
+        remove_list.append(i)
+    config.education_index = np.delete(config.education_index, remove_list, axis=1)
+    remove_list = []
+
+    # 处理非居民专利指数
+    for i in range(4, 15):
+        remove_list.append(1990+i)
+    config.patents_per_capitan = np.nan_to_num(config.patents_per_capitan, nan=0)
+    # config.patents_per_capitan = np.delete(config.patents_per_capitan, remove_list, axis=1)
+
+
+    remove_list = []
+
+    # 处理人均耗电指数
+
+    # 处理人口指数
+    # 处理单位人口指数
+    # 处理人均水资源指数
+    # 处理人均森林面积指数
+'''
