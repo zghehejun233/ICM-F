365c841ed40ff09a94ab205ac7a3831944ff36e7
guosurui <13012615613@163.com>
2022-02-18 16:15:00 +0800

    feat: 添加资源水平子指标的定义和矩阵
    添加资源水平的四个子指标数组及其表示矩阵和国家的矩阵

--- src/config.py
+++ src/config.py
@@ -18,3 +18,23 @@ education_index = np.array([])
 technology_index = np.array([])
 population = np.array([])
 
+# 定义资源水平的四个子指标及其矩阵
+
+arable_land_per_capitan = np.array([])
+water_per_capitan = np.array([])
+forest_per_capitan = np.array([])
+energy_per_capitan = np.array([])
+
+
+def get_resource_matrix():
+    resource_matrix = np.vstack((arable_land_per_capitan, water_per_capitan, forest_per_capitan, energy_per_capitan))
+    return resource_matrix
+
+
+# 定义该国家的年份-指标矩阵
+# 此处定义了一个函数，用来实现矩阵的刷新，保证矩阵由该文件自管理
+
+def get_country_matrix():
+    country_matrix = np.vstack((happiness_index, development_index, resource_index, labor_index, economic_index,
+                                education_index, technology_index, population))
+    return country_matrix
