b629fdccd58f49c4c2c344620db27c03ae58de1b
guosurui <13012615613@163.com>
2022-02-18 16:01:13 +0800

    feat: 添加八个指标数组
    config中添加八个指标数组

--- src/config.py
+++ src/config.py
@@ -4,3 +4,17 @@
 这个文件用来定义全局变量和其他属性
 
 """
+
+import numpy as np
+
+# 定义八个指数数组
+
+happiness_index = np.array([])
+development_index = np.array([])
+resource_index = np.array([])
+labor_index = np.array([])
+economic_index = np.array([])
+education_index = np.array([])
+technology_index = np.array([])
+population = np.array([])
+
