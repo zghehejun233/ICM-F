15b7aad205acefe803610877cfa01dae036bbb12
guosurui <13012615613@163.com>
2022-02-19 13:09:51 +0800

    feat: 完成因子计算
    完成各个因子的计算

--- src/global_equity_model/global_equity_model.py
+++ src/global_equity_model/global_equity_model.py
@@ -5,10 +5,8 @@
 
 """
 
-import logging
 import numpy as np
 import pandas as pd
-from scipy import optimize
 
 np.seterr(divide='ignore', invalid='ignore')
 
@@ -25,7 +23,6 @@ def topsis(data, list_max, list_min):
     for i in range(data.shape[0]):
         max_sum = 0
         min_sum = 0
-        print(data.shape)
         for k in range(data.shape[1]):
             max_sum += np.power(data[i, k] - list_max[k], 2)
             min_sum += np.power(data[i, k] - list_min[k], 2)
@@ -34,7 +31,7 @@ def topsis(data, list_max, list_min):
         answer_list.append(min_list[i] / (min_list[i] + max_list[i]))
         max_sum, min_sum = 0, 0
     answer = np.array(answer_list)
-    print(answer)
+    return answer
 
 
 def technology_modify_function(data):
@@ -44,122 +41,161 @@ def technology_modify_function(data):
 
 
 def resource_modify_function(data):
-    list_max = np.vstack((np.max(data[:, 0]), np.max(data[:, 1]), np.max(data[:, 2])))
-    list_min = np.vstack((np.min(data[:, 0]), np.min(data[:, 1]), np.min(data[:, 2])))
+    list_max = np.vstack((np.max(data[:, 0]), np.max(data[:, 1])))
+    list_min = np.vstack((np.min(data[:, 0]), np.min(data[:, 1])))
     return topsis(data, list_max, list_min)
 
 
+def get_final_index(inc, res, lab, eco, edu, tech, pop):
+    a = 1
+    b = 1
+    c = 1
+    d = 1
+    e = 1
+    f = 1
+    g = 1
+    h = 1
+    final_index = np.array(a * inc + b * res + c * lab + d * eco + e * edu + f * eco + g * tech + h * pop)
+    final_index_except_america = final_index[:-1,:]
+    for i in range(final_index.shape[1]):
+        final_index[:,i] = maximum_normalization(final_index[:,i])
+    for i in range(final_index_except_america.shape[1]):
+        final_index_except_america[:,i]=maximum_normalization(final_index_except_america[:,i])
+    return final_index
+
+
 def start():
-    human_development_file = pd.read_csv("/Users/guosurui/Documents/git_code/ICM-F/data/global_equity_model/人类发展指数.csv")
-    development_index = human_development_file.values
+    income_file = pd.read_csv('/Users/guosurui/Documents/git_code/ICM-F/data/global_equity_model/人均国民净收入额筛选数据.csv')
+    temp = income_file.values
+    income_index = temp[:, 27:].astype(np.float64)
 
-    labor_file = pd.read_csv("/Users/guosurui/Documents/git_code/ICM-F/data/global_equity_model/劳动力.csv")
-    labor_index = labor_file.values
+    labor_file = pd.read_csv("/Users/guosurui/Documents/git_code/ICM-F/data/global_equity_model/劳动力筛选数据.csv",
+                             encoding='utf-8')
+    temp = labor_file.values
+    labor_index = temp[:, 7:].astype(np.float64)
 
-    gdp_file = pd.read_csv("/Users/guosurui/Documents/git_code/ICM-F/data/global_equity_model/GDP_world_bank.csv")
-    economic_index = gdp_file.values
+    gdp_file = pd.read_csv("/Users/guosurui/Documents/git_code/ICM-F/data/global_equity_model/GDP数据筛选数据.csv")
+    temp = gdp_file.values
+    economic_index = temp[:, 37:].astype(np.float64)
 
     high_education_file = pd.read_csv('/Users/guosurui/Documents/git_code/ICM-F/data/global_equity_model'
-                                      '/高等教育入学率.csv')
+                                      '/高等教育入学率筛选数据.csv')
     temp = high_education_file.values
-    education_index = temp[:, 3:].astype(np.float64)
+    education_index = temp[:, 27:].astype(np.float64)
 
-    non_resident_patent = pd.read_csv("/Users/guosurui/Documents/git_code/ICM-F/data/global_equity_model/非居民专利数.csv")
+    non_resident_patent = pd.read_csv(
+        "/Users/guosurui/Documents/git_code/ICM-F/data/global_equity_model/非居民专利数筛选数据.csv")
     temp = non_resident_patent.values
-    patents_per_capitan = temp[:, 3:].astype(np.float64)
+    patents_per_capitan = temp[:, 12:].astype(np.float64)
 
-    power_used = pd.read_csv("/Users/guosurui/Documents/git_code/ICM-F/data/global_equity_model/耗电量.csv")
+    power_used = pd.read_csv("/Users/guosurui/Documents/git_code/ICM-F/data/global_equity_model/耗电量筛选数据.csv")
     temp = power_used.values
-    electricity_per_capitan = temp[:, 3:].astype(np.float64)
+    electricity_per_capitan = temp[:, 21:].astype(np.float64)
 
-    population_file = pd.read_csv('/Users/guosurui/Documents/git_code/ICM-F/data/global_equity_model/人口.csv')
-    population = population_file.values
+    high_technology_income_file = pd.read_csv(
+        "/Users/guosurui/Documents/git_code/ICM-F/data/global_equity_model/高科技出口（美元）筛选数据.csv")
+    temp = high_technology_income_file.values
+    high_technology_income = temp[:, 3:].astype(np.float64)
+
+    population_file = pd.read_csv('/Users/guosurui/Documents/git_code/ICM-F/data/global_equity_model/人口筛选数据.csv')
+    temp = population_file.values
+    population_index = temp[:, 37:].astype(np.float64)
 
     population_per_city_file = pd.read_csv("/Users/guosurui/Documents/git_code/ICM-F/data/global_equity_model"
-                                           "/城市人口比例.csv")
+                                           "/城镇人口比例筛选数据.csv")
     temp = population_per_city_file.values
-    population_density_per_city = temp[:, 3:].astype(np.float64)
+    population_density_per_city = temp[:, 32:].astype(np.float64)
 
-    # 数据拟合不好，先不看
-    water_file = pd.read_csv("/Users/guosurui/Documents/git_code/ICM-F/data/global_equity_model/人均可再生内陆水资源.csv")
-    temp = water_file.values
-    water_per_capitan = temp[:, 6:].astype(np.float64)
-
-    forest_file = pd.read_csv("/Users/guosurui/Documents/git_code/ICM-F/data/global_equity_model/森林面积.csv")
+    forest_file = pd.read_csv("/Users/guosurui/Documents/git_code/ICM-F/data/global_equity_model/森林面积筛选数据.csv")
     temp = forest_file.values
-    forest_per_capitan = temp[:, 3:].astype(np.float64)
+    forest_per_capitan = temp[:, 8:].astype(np.float64)
 
     print('导入数据结束')
 
     # 清洗数据
 
-    development_index = np.nan_to_num(development_index, nan=0)
+    income_index = np.nan_to_num(income_index, nan=0)
     labor_index = np.nan_to_num(labor_index, nan=0)
     economic_index = np.nan_to_num(economic_index, nan=0)
     education_index = np.nan_to_num(education_index, nan=0)
-    population = np.nan_to_num(population, nan=0)
+    population_index = np.nan_to_num(population_index, nan=0)
 
     patents_per_capitan = np.nan_to_num(patents_per_capitan, nan=0.0)
     electricity_per_capitan = np.nan_to_num(electricity_per_capitan, nan=0)
 
     population_density_per_city = np.nan_to_num(population_density_per_city, nan=0)
-    water_per_capitan = np.nan_to_num(water_per_capitan, nan=0)
     forest_per_capitan = np.nan_to_num(forest_per_capitan, nan=0)
-    print(population_density_per_city)
-    print(water_per_capitan)
-    print(forest_per_capitan)
     print('清理nan数据结束')
 
     # 定义幸福度指数计算
-    # 定义发展指数计算
-    # 定义资源水平指数计算
-    # 城市人口比例逐行归一化
+    for i in range(income_index.shape[1]):
+        income_index[:, i] = maximum_normalization(income_index[:, i])
+
+    # 资源水平计算 城市人口比例逐行归一化
     for i in range(population_density_per_city.shape[1]):
         population_density_per_city[:, i] = maximum_normalization(population_density_per_city[:, i])
-    # 人均可再生水逐行归一化
-    for i in range(water_per_capitan.shape[1]):
-        water_per_capitan[:, i] = maximum_normalization(water_per_capitan[:, i])
     # 人均森林面积逐行归一化
     for i in range(forest_per_capitan.shape[1]):
         forest_per_capitan[:, i] = maximum_normalization(forest_per_capitan[:, i])
-    temp_matrix = np.vstack((population_density_per_city[:124, 0], water_per_capitan[:124, 0]))
+    # 构建资源矩阵
+    temp_matrix = np.vstack((population_density_per_city[:, 0], forest_per_capitan[:, 0]))
     resource_matrix = temp_matrix.transpose()
-    for i in range(1, 30):
+    for i in range(1, 25):
         temp_matrix = np.vstack(
-            (population_density_per_city[:124, i], water_per_capitan[:124, i], forest_per_capitan[:124, i]))
-        temp_matrix = temp_matrix.transpose()
-        resource_matrix = np.column_stack((resource_matrix, temp_matrix))
-    resource_matrix = resource_matrix.reshape((29, 124, 3))
+            (population_density_per_city[:, i], forest_per_capitan[:, i]))
+        resource_matrix = np.hstack((resource_matrix, temp_matrix.transpose()))
+    # 计算指数
+    temp_data = resource_matrix[:, :2]
+    answer = resource_modify_function(temp_data)
+    resource_index = np.array(answer)
+    for i in range(25):
+        temp_data = resource_matrix[:, 2 * i:2 * (i + 1)]
+        answer = resource_modify_function(temp_data)
+        resource_index = np.hstack((resource_index, answer))
 
     # 定义劳动力水平指数计算
+    for i in range(labor_index.shape[1]):
+        labor_index[:, i] = maximum_normalization(labor_index[:, i])
     # 定义经济水平指数计算
+    for i in range(economic_index.shape[1]):
+        economic_index[:, i] = maximum_normalization(economic_index[:, i])
+
     # 定义教育水平指数计算
-    # 科技发展水平指数计算
-    # 非居民专利数逐行归一化数据
+    for i in range(education_index.shape[1]):
+        education_index[:, i] = maximum_normalization(education_index[:, i])
+
+    # 科技水平 非居民专利数逐行归一化数据
     for i in range(patents_per_capitan.shape[1]):
         patents_per_capitan[:, i] = maximum_normalization(patents_per_capitan[:, i])
     # 取对数
-    for i in range(patents_per_capitan.shape[0]):
-        for j in range(patents_per_capitan.shape[1]):
-            if patents_per_capitan[i, j] != 0:
-                patents_per_capitan[i, j] = np.log(patents_per_capitan[i, j])
+    for i in range(electricity_per_capitan.shape[0]):
+        for j in range(electricity_per_capitan.shape[1]):
+            if electricity_per_capitan[i, j] != 0:
+                electricity_per_capitan[i, j] = np.log(electricity_per_capitan[i, j])
     # 人均耗电量逐行归一化数据
     for i in range(electricity_per_capitan.shape[1]):
         electricity_per_capitan[:, i] = maximum_normalization(electricity_per_capitan[:, i])
-    # 构建并转置矩阵
-    temp_matrix = np.vstack((electricity_per_capitan[:124, 0], patents_per_capitan[:124, 0]))
+    # 构建资源矩阵
+    temp_matrix = np.vstack((patents_per_capitan[:, 0], electricity_per_capitan[:, 0]))
     technology_matrix = temp_matrix.transpose()
-    # 构建高维矩阵
-    for i in range(1, 40):
-        temp_matrix = np.vstack((electricity_per_capitan[:124, i], patents_per_capitan[:124, i]))
-        temp_matrix = temp_matrix.transpose()
-        technology_matrix = np.column_stack((technology_matrix, temp_matrix))
-    technology_matrix = technology_matrix.reshape((40, 124, 2))
-
-    # topsis分析测试
-    test_data = maximum_normalization(np.random.randn(20)).reshape(10, 2)
-    print(test_data)
-    technology_modify_function(test_data)
+    for i in range(1, 25):
+        temp_matrix = np.vstack(
+            (patents_per_capitan[:, i], electricity_per_capitan[:, i]))
+        technology_matrix = np.hstack((resource_matrix, temp_matrix.transpose()))
+    # 计算指数
+    temp_data = technology_matrix[:, :2]
+    answer = technology_modify_function(temp_data)
+    technology_index = np.array(answer)
+    for i in range(25):
+        temp_data = technology_matrix[:, 2 * i:2 * (i + 1)]
+        answer = technology_modify_function(temp_data)
+        technology_index = np.hstack((technology_index, answer))
 
     # 定义人口水平指数计算
-    #
+    for i in range(population_index.shape[1]):
+        population_index[:, i] = maximum_normalization(population_index[:, i])
+
+    print('因子计算结束')
+    get_final_index(income_index, resource_index, labor_index, economic_index, education_index, technology_index,
+                    population_index)
+    print('全球公平指数生成')
